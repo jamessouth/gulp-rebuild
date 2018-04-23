7f3bb71f366f3593bfa6a2cbc018042b0f5804e9
jamessouth <jamesdanielsouth@gmail.com>
2018-04-23 01:18:07 -0500

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const gulp = require('gulp'),
   imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
       sass = require('gulp-sass'),
+  greplace = require('gulp-replace'),
+     serve = require('gulp-serve'),
       csso = require('gulp-csso'),
       maps = require('gulp-sourcemaps'),
        del = require('del');
 
 
-gulp.task('prepScripts', ['clean'], () => {
+gulp.task('prepScripts', () => {
   return gulp.src(['js/circle/jquery-3.3.1.js', 'js/circle/circle.js', 'js/circle/autogrow.js'])
   .pipe(maps.init())
   .pipe(concat('global.js'))
@@ -24,7 +26,7 @@ gulp.task('scripts', ['prepScripts'], () => {
   .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('prepStyles', ['clean'], () => {
+gulp.task('prepStyles', () => {
   return gulp.src('sass/global.scss')
   .pipe(maps.init())
   .pipe(sass())
@@ -39,7 +41,7 @@ gulp.task('styles', ['prepStyles'], () => {
   .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('images', ['clean'], () => {
+gulp.task('images', () => {
   return gulp.src('images/*')
   .pipe(imagemin())
   .pipe(gulp.dest('dist/content'));
@@ -48,15 +50,53 @@ gulp.task('images', ['clean'], () => {
 
 
 gulp.task('clean', () => {
-  return del('dist/*');
+  return del(['dist/*', 'js/global.js*', 'css/*']);
+});
+
+gulp.task('copyIcons', () => {
+  gulp.src('icons/**', {base: '.'})
+  .pipe(gulp.dest('dist'));
 });
 
 
-gulp.task('build', ['scripts', 'styles', 'images']);
+gulp.task('copyHTML', () => {
+  gulp.src('index.html')
+  .pipe(greplace(/images/g, 'content'))
+  .pipe(greplace('css/global.css', 'styles/all.min.css'))
+  .pipe(greplace('js/global.js', 'scripts/all.min.js'))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('preBuild', ['scripts', 'styles', 'images', 'copyIcons', 'copyHTML']);
+
+gulp.task('build', ['clean'], () => {
+  gulp.start('preBuild');
+});
+
+gulp.task('reload', ['styles'], () => {
+  gulp.start('serve');
+  setTimeout(() => {
+    console.log();
+    console.log('The server has been restarted.');
+    console.log();
+  }, 1000);
+});
+
+gulp.task('watchSass', () => {
+  gulp.watch('sass/**/*.s*', ['reload']);
+});
+
+gulp.task('serve', serve({
+    root: 'dist',
+    port: 3000
+}));
 
 gulp.task('default', ['build'], () => {
-  gulp.src(['icons', 'index.html'])
-  .pipe(gulp.dest('dist'))
-  .pipe(gulp.src('icons/**'))
-  .pipe(gulp.dest('dist/icons'));
+  gulp.start('serve');
+  gulp.start('watchSass');
+  setTimeout(() => {
+    console.log();
+    console.log('Server is running on localhost:3000.  Press Ctrl-C to stop.  Thank you for building with gulp and have a nice day!');
+    console.log();
+  }, 1000);
 });
